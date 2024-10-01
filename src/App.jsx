eb360830ebf54babb066a8397b3dd2b71e8bc121
Mohammed23-rizwan <rizuart23@gmail.com>
2024-10-01 16:19:22 +0530

    added shadows

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   const [click, setClick] = useState("default value");
   return (
     <div className="grid place-items-center w-full h-screen">
-      <div className="container w-[350px] h-[150px] border border-blue-300 rounded-[5px] flex flex-col items-center">
+      <div className="container w-[350px] h-[150px] border border-blue-300 rounded-[5px] flex flex-col items-center shadow-[0px_4px_16px_rgba(17,17,26,0.1),_0px_8px_24px_rgba(17,17,26,0.1),_0px_16px_56px_rgba(17,17,26,0.1)]">
         <h2 className="text-center mt-3 text-red-300 text-lg">
           Welcome to{" "}
           <span className="text-red-500 text-lg uppercase">{click}</span>
